6dede96dcb67349520e7806bae5dbdc7b2e3a1a4
Terese Haimberger <tvhtoo@gmail.com>
2022-01-09 12:10:21 +0100

    Get reply data from props, not state

--- client/src/components/Replies.tsx
+++ client/src/components/Replies.tsx
@@ -9,39 +9,30 @@ interface Props {
   onMoreClick: (parentName: string, commentIDs: string[]) => void;
 }
 
-class Replies extends React.Component<Props> {
-  state: Props;
-
-  constructor(props: Props) {
-    super(props);
-    this.state = props;
-  }
-
-  render() {
-    const html = this.state.data.map(reply => {
-      switch (reply.kind) {
-      case 'Comment': {
-        return <Comment
-          key={reply.id}
-          data={reply as models.Comment}
-          onMoreClick={this.props.onMoreClick}
-        />;
-        break;
-      }
-      case 'MoreComments': {
-        const more = reply as models.MoreComments;
-        if (!more.count) return null;
-        return <MoreComments
-          key={more.id}
-          data={more}
-          onMoreClick={this.props.onMoreClick}
-        />;
-        break;
-      }
-      }
-    });
-    return <div>{html}</div>;
-  }
+function Replies(props: Props) {
+  const html = props.data.map(reply => {
+    switch (reply.kind) {
+    case 'Comment': {
+      return <Comment
+        key={reply.id}
+        data={reply as models.Comment}
+        onMoreClick={props.onMoreClick}
+      />;
+      break;
+    }
+    case 'MoreComments': {
+      const more = reply as models.MoreComments;
+      if (!more.count) return null;
+      return <MoreComments
+        key={more.id}
+        data={more}
+        onMoreClick={props.onMoreClick}
+      />;
+      break;
+    }
+    }
+  });
+  return <div>{html}</div>;
 }
 
 export default Replies;
